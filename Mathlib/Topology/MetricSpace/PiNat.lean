cf6dad6d0ab7b252abca6ee6f759f25b3e5947c8
FormulaRabbit81 <arav.bhattacharyya21@imperial.ac.uk>
2025-09-03 16:40:18 +0100

    feat(Topology): obtain new distance structures on pseudo(e)metricspaces

--- Mathlib/Topology/MetricSpace/PiNat.lean
+++ Mathlib/Topology/MetricSpace/PiNat.lean
@@ -48,6 +48,53 @@ in general), and `ι` is countable.
   the uniformity is definitionally the product uniformity. Not registered as an instance.
 -/
 
+namespace PseudoMetricSpace
+variable {X : Type*}
+
+/-- Build a new pseudometric space from an old one where the distance uniform structure is provably
+(but typically non-definitionally) equal to some given distance structure. -/
+-- See note [forgetful inheritance]
+-- See note [reducible non-instances]
+abbrev replaceDist (m : PseudoMetricSpace X) (d : X → X → ℝ) (hd : d = dist) :
+    PseudoMetricSpace X where
+  dist := d
+  dist_self := by simp [hd]
+  dist_comm := by simp [hd, dist_comm]
+  dist_triangle := by simp [hd, dist_triangle]
+  edist_dist := by simp [hd, edist_dist]
+  uniformity_dist := by simp [hd, uniformity_dist]
+  cobounded_sets := by simp [hd, cobounded_sets]
+  __ := m
+
+lemma replaceDist_eq (m : PseudoMetricSpace X) (d : X → X → ℝ) (hd : d = dist) :
+    m.replaceDist d hd = m := by ext : 2; exact hd
+
+end PseudoMetricSpace
+
+namespace PseudoEMetricSpace
+
+/-- One gets a pseudometric space from an emetric space if the edistance
+is everywhere finite, by pushing the edistance to reals. We set it up so that the edist and the
+uniformity are defeq in the pseudometric space and the pseudoemetric space. In this definition, the
+distance is given separately, to be able to prescribe some expression which is not defeq to the
+push-forward of the edistance to reals. See note [reducible non-instances]. -/
+abbrev toPseudoMetricSpaceOfDist' {X : Type*} [e : PseudoEMetricSpace X] (dist : X → X → ℝ)
+    (dist_nonneg : ∀ x y, 0 ≤ dist x y)
+    (h : ∀ x y, edist x y = .ofReal (dist x y)) : PseudoMetricSpace X where
+  dist := dist
+  dist_self x := by simpa [h, (dist_nonneg _ _).ge_iff_eq', -edist_self] using edist_self x
+  dist_comm x y := by simpa [h, dist_nonneg] using edist_comm x y
+  dist_triangle x y z := by
+    simpa [h, dist_nonneg, add_nonneg, ← ENNReal.ofReal_add] using edist_triangle x y z
+  edist := edist
+  edist_dist _ _ := by simp only [h]
+  toUniformSpace := toUniformSpace
+  uniformity_dist := e.uniformity_edist.trans <| by
+    simpa [h, dist_nonneg, ENNReal.coe_toNNReal_eq_toReal]
+      using (Metric.uniformity_edist_aux fun x y : X => (edist x y).toNNReal).symm
+
+end PseudoEMetricSpace
+
 noncomputable section
 
 open Topology TopologicalSpace Set Metric Filter Function
